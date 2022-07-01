a92b16dfad35b7a89f618fe02e6d9778352366c8
IMANZE <97724382+IMANZE@users.noreply.github.com>
2022-07-01 19:37:38 +0200

    Solved exercise

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import MyBadge from './MyBadge';
+import MyBadge from './component/MyBadge';
+import SingleBook from './component/SingleBook';
+import BookList from './component/BookList';
+import WarningSign from './component/WarningSign';
+import fantasyBooks from './fantasyBooks.json'
 
 function App() {
   return (
     <div>
        <MyBadge title="Welcome to Ezi BookStore" color="primary"/>
+       <SingleBook book={fantasyBooks[0]} />
+       <BookList books={fantasyBooks} />
+       <WarningSign text="Watch out again!" />
     </div>
   );
 }
